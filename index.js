e58f9838430408fb47e1bee83942a8a5300b97c7
Thalles Velasques <thalles.velasques123@gmail.com>
2024-09-09 21:12:38 -0300

    fundamentos 001

---
+++ index.js
@@ -0,0 +1,14 @@
+// Imprimindo "Hello World!"
+
+let mensagem = "Hello World!"
+/*
+let - variavel que pode mudar
+const - variavel que nao muda
+*/
+
+{
+    const mensagem = "Ola mundo!"
+    console.log(mensagem);
+}
+
+console.log(mensagem);
