aa6bfd21e3e449ee994b87b300a9aa335e8d74e2
Thalles Velasques <thalles.velasques123@gmail.com>
2024-09-09 22:28:28 -0300

    fundamentos 002

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Imprimindo "Hello World!"
-
-let mensagem = "Hello World!"
 /*
-let - variavel que pode mudar
-const - variavel que nao muda
-*/
+let mensagem = "Hello World!"
+
+// let - variavel que pode mudar
+// const - variavel que nao muda
+
 
 {
     const mensagem = "Ola mundo!"
@@ -12,3 +12,41 @@ const - variavel que nao muda
 }
 
 console.log(mensagem);
+*/
+
+// arrays, objetos
+/*
+let metas = ["Thalles", "Alo"]
+
+console.log(metas[1] + ", " + metas[0])
+*/
+
+ /*
+let meta = {
+    value: "ler um livro por mês",
+    checked: false,
+
+}
+console.log(meta.value)
+*/
+
+
+let meta = {
+    value: "ler um livro por mês",
+    checked: false, 
+    log: (info) =>{
+        console.log(info)
+    }
+
+}
+
+meta.value = "não é mais ler um livro"
+meta.log(meta.value)
+
+
+/*
+// function | arrow function
+const criarMeta = () => {}
+
+function criarMeta() {}
+*/
