01380ea3b6daf330010b12dd039713989124689d
Thalles Velasques <thalles.velasques123@gmail.com>
2024-09-10 22:06:36 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require("@inquirer/prompts")
+const { select, input, checkbox } = require("@inquirer/prompts")
 
 let metas = []
 
@@ -13,6 +13,33 @@ async function cadastrarMeta (){
     metas.push( 
         { value: meta, checked: false }
     )
+}
+
+async function listarMetas(){
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa.",
+        choices: [...metas],
+        instructions: false
+    })
+
+    if(respostas.length == 0){
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+        
+        meta.checked = true
+    })
+
+    console.log("Meta(s) marcada(s) como concluída(s)")
 }
 
 async function start (){
@@ -43,7 +70,7 @@ async function start (){
                 console.log(metas)
                 break
             case "listar":
-                console.log("Vamos listar");
+                await listarMetas()
                 break
             case "sair":
                 console.log("Até a proxima!")
